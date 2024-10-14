6112b108c391fc8dc945832755336982f0652e46
parsehex <1779937+parsehex@users.noreply.github.com>
2024-10-14 15:29:41 -0400

    add elevenlabs tts model enum

--- src/enums.py
+++ src/enums.py
@@ -1,5 +1,13 @@
 from enum import Enum
 
+
+class ElevenLabsTTSModel(Enum):
+	Turbo_v25 = 'eleven_turbo_v2_5'
+	Turbo_v2 = 'eleven_turbo_v2'
+	Multilingual_v2 = 'eleven_multilingual_v2'
+	Monolingual_v1 = 'eleven_monolingual_v1'
+
+
 class TogetherAIFluxModel(Enum):
 	Flux1SchnellFree = 'black-forest-labs/FLUX.1-schnell-Free'
 	Flux1SchnellTurbo = 'black-forest-labs/FLUX.1-schnell'
