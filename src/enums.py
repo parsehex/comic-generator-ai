4ac4432129dae6baa9fb053b4305c94a172f52f0
parsehex <1779937+parsehex@users.noreply.github.com>
2024-10-14 13:06:20 -0400

    add img_gen notebook

---
+++ src/enums.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class TogetherAIFluxModel(Enum):
+	Flux1SchnellFree = 'black-forest-labs/FLUX.1-schnell-Free'
+	Flux1SchnellTurbo = 'black-forest-labs/FLUX.1-schnell'
+	Flux1_1Pro = 'black-forest-labs/FLUX.1.1-pro'
+	Flux1Pro = 'black-forest-labs/FLUX.1-pro'
